82c6be8b4fb98a9cadc0f92364abc24526ab8a44
jinhyungrhee <rheejinhyung@gmail.com>
2022-12-19 17:58:35 +0900

    221219 - chap11 컬렉션 프레임워크

--- chap11/src/chap11/ArrayListTest.java
+++ chap11/src/chap11/ArrayListTest.java
@@ -28,11 +28,19 @@ public class ArrayListTest {
 
 	public static void main(String[] args) {
 
-		//상속관계가 없는 서로다른 클래스의 객체를 한 공간에 저장 가능!
-		ArrayList list = new ArrayList(5); // 5개 저장 공간 -> 크기 고정이 아닌 동적으로 변함!
-		list.add("java"); // 참조형 변수
-		list.add(10); // auto-boxing
-		list.add(3.14); // auto-boxing
+		// 동일 타입 데이터만 저장 강제적 규칙. 동적 크기
+		
+		// Employee 타입의 ArrayList
+		// ArrayLiust Employee 타입 매개변수 유사 전달
+		// ArrayList 저장 타입 제한
+		// => generics
+		// 1. 컴파일 타입 체크
+		// 2. 형변환 코드 불필요
+		// 3. add(Object o) / Object <- get()
+		ArrayList<Employee> list = new ArrayList<>(5);
+		//list.add("java"); // 참조형 변수
+		//list.add(10); // auto-boxing
+		//list.add(3.14); // auto-boxing
 		
 		Employee e1 = new Employee(100, "이사원", 1000000); 
 		Employee e2 = new Employee(200, "최대리", 1500000); 
@@ -41,12 +49,20 @@ public class ArrayListTest {
 		list.add(e1);
 		list.add(e2);
 		list.add(e3);
+	
+//		for(int i = 0; i < list.size(); i++) {
+//			if (list.get(i) instanceof Employee) {
+//				System.out.println(i + "번째 위치 = " + ((Employee)list.get(i)).name);
+//			}
+//			
+//			// Employee name만 출력 
+//			// 문제1. Employee가 아닌 것들도 존재(name변수 없는 객체)
+//		
+//		}
 		
-		System.out.println(list.size()); // 6
-		
-		for(int i = 0; i < list.size(); i++) {
-			System.out.print(list.get(i).getClass().getName() + " : ");
-			System.out.println(list.get(i)); // toString() 호출 
+		for (int i = 0; i< list.size(); i++) {
+			System.out.println(i + "번째 위치 = " + list.get(i));
+			// Employee name만 입력
 		}
 		
 	}
